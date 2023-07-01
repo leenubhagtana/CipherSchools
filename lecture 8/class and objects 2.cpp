f06360c6ac8113a551ae5de25325e80e0e00c5cc
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2023-07-01 12:48:01 +0530

    Add files via upload

---
+++ lecture 8/class and objects 2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    string passcode;
+public:
+    string name;
+    int id;
+    student()
+    {
+    }
+    student(string passcode,string name,int id)
+    {
+       this-> name =name;
+        this->id =id;
+        this->passcode=passcode;
+    }
+    void into()
+    {
+        cout<<"My name is "<<name<<"Id is "<<id<<"passcode is "<<passcode;
+    }
+    void setpass(string s)
+    {
+        passcode=s;
+    }
+};
+int main()
+{
+    student s1;
+    student s2("10","mohit",1);
+    s2.into();
+
+    return 0;
+}
